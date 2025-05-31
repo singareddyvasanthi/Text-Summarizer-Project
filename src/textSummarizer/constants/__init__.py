47a9a9ce9643887e2bc5155f9e92b30e1d705844
vasanthi <ADMIN@DESKTOP-5CSS8C3.(none)>
2025-05-31 12:08:01 +0530

    stage1 created data ingestion

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+#here we are definfing the values which are constant
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,11 @@
+# It is an entity it will return the values
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
